662ee8ce8fbea67696f6ceb4c46aef16aff36a4c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-08 16:22:17 +0100

    Added assets loading and more assets

--- assets/javascript/classes/sprite.js
+++ assets/javascript/classes/sprite.js
@@ -5,9 +5,11 @@
  */
 var Sprite = new Class({
 	Extends: Rectangle,
-	initialise: function(options, path, callback) {
+	initialize: function(options, path, callback) {
 		// Pass the options up to the Rectangle class
 		this.parent(options);
+		
+		this.loaded = false;
 		
 		// If there is a path, load an image
 		if(path) {
@@ -17,14 +19,19 @@ var Sprite = new Class({
 	loadImage: function(path, callback) {
 		// Initialise the image and set the onload listener
 		var temp = new Image();
+		this.loaded = false;
 		
-		temp.onLoad = function() {
+		temp.onload = function() {
 			// When done loading, store the image and run the callback
 			this.image = new Element('canvas');
 			this.image.width = this.size.x;
 			this.image.height = this.size.y;
-			this.image.drawImage(temp, this.position.x, this.position.y, this.size.x, this.size.y);
-			callback.call(this);
+			this.image.getContext('2d').drawImage(temp, this.position.x, this.position.y, this.size.x, this.size.y);
+			this.loaded = true;
+			
+			if(callback) {
+				callback.call(this);
+			}
 		}.bind(this);
 		
 		// Set the src of the image to begin loading
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,67 @@
+var sprites = {},
+	display = null;
+
 /**
  * Main script, runs basically everything on DOM ready
  */
 document.addEvent('domready', function() {
-	// Set up page management
+	// Load the sprites
+	sprites.cat = new Sprite({
+		size: {
+			x: 80,
+			y: 50
+		},
+		position: {
+			x: 0,
+			y: 0
+		}
+	}, 'assets/images/nyan-cat.png', setUpNav);
+	
+	sprites.wall = new Sprite({
+		size: {
+			x: 30,
+			y: 500
+		},
+		position: {
+			x: 0,
+			y: 0
+		}
+	}, 'assets/images/wall.png', setUpNav);
+	
+	sprites.rainbow = new Sprite({
+		size: {
+			x: 3,
+			y: 12
+		},
+		position: {
+			x: 0,
+			y: 0
+		}
+	}, 'assets/images/rainbow.png', setUpNav);
+	
+	display = $('display').getContext('2d');
+});
+
+function setUpNav() {
+	// Make sure all are loaded
+	if(!sprites.cat.loaded || !sprites.wall.loaded || !sprites.rainbow.loaded) {
+		return false
+	}
+	
+	// Loaded, set up page management
 	var mainNav = new Navigation(function(from, to) {
 		if(from) {
 			$('page:' + from).addClass('hidden');
 		}
-		
+
 		if(to) {
 			$('page:' + to).removeClass('hidden');
 		}
 	});
-	
+
 	mainNav.checkTag('menu');
-	
+
 	$$('a[data-page]').addEvent('click', function() {
 		mainNav.checkTag(this.get('data-page'));
 	});
-});
+}
