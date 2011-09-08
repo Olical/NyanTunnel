20b8310fbbc73b9a9b34c706aec63490d447a7fd
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-08 14:42:15 +0100

    Set up the navigation class

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,5 +2,14 @@
  * Main script, runs basically everything on DOM ready
  */
 document.addEvent('domready', function() {
-	
+	// Set up page management
+	var mainNav = new Navigation(function(from, to) {
+		if(from) {
+			$('page:' + from).addClass('hidden');
+		}
+		
+		if(to) {
+			$('page:' + to).removeClass('hidden');
+		}
+	});
 });
